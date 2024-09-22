2632665d617f1a94e9bdbf3fc2984bbb63f98e31
Dharmesh Shimpi <161144920+Dharmesh-Shimpi@users.noreply.github.com>
2024-09-22 23:17:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screen: {
+        xs: '340px',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
