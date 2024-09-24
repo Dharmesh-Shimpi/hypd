29b49c5d8442c54edc20d233e0479e6ec350026b
Dharmesh Shimpi <161144920+Dharmesh-Shimpi@users.noreply.github.com>
2024-09-24 19:48:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -52,7 +52,7 @@ const ScrollComponent = () => {
   }, [index]);
 
   return (
-    <div className="flex bg-black relative z-10">
+    <div className="flex border bg-black relative z-10">
   {/* Text Section with scrolling */}
   <div className="">
     {textData.map((text, i) => (
